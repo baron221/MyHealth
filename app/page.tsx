6100fb13e67858c2c77feed67eb3d08b84277cba
Baron221 <baronjon080@gmail.com>
2024-07-26 16:27:46 +0900

    feat:PatientForm page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-3xl underline"> Home </h1>
-      </div>
-    </main>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm/>
+        </div>
+      </section>
+    </div>
   );
 }
